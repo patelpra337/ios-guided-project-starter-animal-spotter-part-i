d8fddcef257e2dabd92083da9a1d72c39cd308ea
Pravin <patelpra337@gmail.com>
2020-03-16 22:33:50 -0500

    update TableViewController & LoginViewController

--- AnimalSpotter/View Controllers/AnimalsTableViewController.swift
+++ AnimalSpotter/View Controllers/AnimalsTableViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class AnimalsTableViewController: UITableViewController {
     
     // MARK: - Properties
-    
+    private let apiController = APIController()
     private var animalNames: [String] = []
 
     // MARK: - View Lifecycle
@@ -23,7 +23,9 @@ class AnimalsTableViewController: UITableViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        // transition to login view if conditions require
+        if apiController.bearer == nil {
+            performSegue(withIdentifier: "LoginViewModalSegue", sender: self)
+        }
     }
 
     // MARK: - Table view data source
@@ -51,7 +53,9 @@ class AnimalsTableViewController: UITableViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "LoginViewModalSegue" {
+        if segue.identifier == "LoginViewModalSegue",
+            let loginVC = segue.destination as? LoginViewController {
+            loginVC.apiController = apiController
             // inject dependencies
         }
     }
